fda834d9b9eebcd58d2d17e219ca6303bdd74214
Sir Gallo <nickgallo97@gmail.com>
2025-04-26 19:34:40 -0400

    comments

--- packages/practice/src/binary-search/binary-search.ts
+++ packages/practice/src/binary-search/binary-search.ts
@@ -18,7 +18,7 @@ export const binarySearch = (list: number[], item: number): number | undefined =
 const list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const list2 = [1, 3, 6, 9, 15, 30, 31, 50, 56, 90, 99]
 
-console.log(binarySearch(list1, 8))
-console.log(binarySearch(list1, 3))
-console.log(binarySearch(list2, 5))
-console.log(binarySearch(list2, 56))
+console.log(binarySearch(list1, 8)) // 7
+console.log(binarySearch(list1, 3)) // 2
+console.log(binarySearch(list2, 5)) // undefined
+console.log(binarySearch(list2, 56)) // 8
