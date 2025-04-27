e28fd7edd4d7f184e4c0011618708e4128c810fe
Sir Gallo <nickgallo97@gmail.com>
2025-04-26 20:23:01 -0400

    clean up

--- packages/practice/src/recursion/recursion.ts
+++ packages/practice/src/recursion/recursion.ts
@@ -2,6 +2,11 @@ interface Box {
   items: (Box | string)[]
 }
 
+/*
+  suppose you have box with nested boxes. There is a key in one of the boxes that is a string.
+
+  recurse through the boxes until you find the key
+*/
 export const lookForKeyRecursive = (box: Box): string | undefined => {
   for (const item of box.items) {
     if (typeof item === 'string') return item
@@ -16,17 +21,17 @@ const box: Box = {
     {
       items: [
         { items: [] },
-        'this is the string'
-      ]
-    },
-    {
-      items: [
         { items: [] }
       ]
     },
     {
       items: [
         { items: [] },
+        'this is the string'
+      ]
+    },
+    {
+      items: [
         { items: [] }
       ]
     }
