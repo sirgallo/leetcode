b7dcc806e86bad868f645f40b9732d12dbf12a77
Sir Gallo <nickgallo97@gmail.com>
2025-04-26 20:20:18 -0400

    simple recursion example

---
+++ packages/practice/src/recursion/recursion.ts
@@ -0,0 +1,48 @@
+interface Box {
+  items: (Box | string)[]
+}
+
+export const lookForKeyRecursive = (box: Box): Box | string | undefined => {
+  for (const item of box.items) {
+    if (typeof item === 'string') {
+      return item
+    }
+  
+    const found = lookForKeyRecursive(item)
+    if (found !== undefined) {
+      return found
+    }
+  }
+}
+
+const box: Box = {
+  items: [
+    {
+      items: [
+        {
+          items: []
+        },
+        'this is the string'
+      ]
+    },
+    {
+      items: [
+        {
+          items: []
+        }
+      ]
+    },
+    {
+      items: [
+        {
+          items: []
+        },
+        {
+          items: []
+        }
+      ]
+    }
+  ]
+}
+
+console.log(lookForKeyRecursive(box))
